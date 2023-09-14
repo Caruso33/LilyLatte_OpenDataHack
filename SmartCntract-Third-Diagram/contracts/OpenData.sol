1de8b9a2672ca5feb5b04ace24f9b234f2d0b73b
fabianWeberEstim <fabianWeberestim@gmail.com>
2023-09-14 09:56:59 +0330

    feat:add some changes on sol file

--- SmartCntract-Third-Diagram/contracts/OpenData.sol
+++ SmartCntract-Third-Diagram/contracts/OpenData.sol
@@ -7,16 +7,22 @@ import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Receiver.sol";
 
-contract MyToken is ERC1155, Ownable, Pausable, ERC1155Burnable {
-    
+contract MyToken is ERC1155, Ownable, Pausable, ERC1155Burnable, ERC1155Receiver{
+    using SafeMath for uint256;
     using SafeERC20 for IERC20;
+
     IERC20 public immutable rewardsToken;
     //the amount of reward
     uint256 rewards = 2;
     //the id of NFT can transfer
     uint256 nft_id = 1;
+    
+    
     event Transfer(address indexed from, address indexed to, uint256 value);
+    
     struct DataOwner {
         address wallet;
         string[] tableIds;
@@ -25,6 +31,7 @@ contract MyToken is ERC1155, Ownable, Pausable, ERC1155Burnable {
 
 
     mapping(address => DataOwner) OwnerToData;
+    mapping(uint256 => uint256) tokenPrices;
 
     DataOwner dataOwner;
 
@@ -44,11 +51,11 @@ contract MyToken is ERC1155, Ownable, Pausable, ERC1155Burnable {
         _unpause();
     }
 
-    function mint(uint256 id, uint256 amount, bytes memory data)
+    function mint(address to, uint256 id, uint256 amount, bytes memory data)
         public
         onlyOwner
     {
-        _mint(msg.sender, id, amount, data);
+        _mint(to, id, amount, data);
     }
 
     function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
@@ -58,16 +65,59 @@ contract MyToken is ERC1155, Ownable, Pausable, ERC1155Burnable {
         _mintBatch(to, ids, amounts, data);
     }
 
+    function setApprovalForAll(address operator, bool approved) 
+        public 
+        override
+    {
+        emit ApprovalForAll(msg.sender, operator, approved);
+    }
+    
     function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
         internal
         whenNotPaused
         override
     {
         //Check nft id and the id of nft that client want to transfer
-        if(nft_id != ids[0]) {
+        if (from == address(this) || (nft_id == ids[0] && ids.length == 1)) {
+            super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
+        }else {
             require(from == address(0) || to == address(0), "This a Soulbound token. It cannot be transferred. It can only be burned by the token owner.");
         }
-        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
+        
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override(ERC1155, ERC1155Receiver) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function onERC1155Received(address operator, address from, uint256 id, uint256 value, bytes calldata data) public override returns(bytes4) {
+        return this.onERC1155Received.selector;
+    }
+
+    function onERC1155BatchReceived(address operator, address from, uint256[] calldata ids, uint256[] calldata values, bytes calldata data) public override returns(bytes4) {
+        return this.onERC1155BatchReceived.selector;
+    }
+
+    function setTokenPrice(uint256 id, uint256 price) 
+        public
+        onlyOwner
+    {
+        tokenPrices[id] = price * 1 ether;
+    }
+
+    function buyToken(uint256 id, uint256 amount) 
+        public 
+        payable
+    {
+        require(msg.value >= tokenPrices[id].mul(amount), "Insufficient funds");
+        _safeTransferFrom(address(this), msg.sender, id, amount, "");
+    }
+
+    function withdraw()
+        public
+        onlyOwner
+    {
+        payable(owner()).transfer(address(this).balance);
     }
 
     function storeTableId(address wallet, string memory id)
@@ -78,13 +128,23 @@ contract MyToken is ERC1155, Ownable, Pausable, ERC1155Burnable {
        OwnerToData[wallet] = dataOwner;
     }
 
-    function readAllDaoMemberTableIds(address wallet) public onlyOwner view returns(string[] memory) {
+    function readAllDaoMemberTableIds(address wallet) 
+        public 
+        onlyOwner 
+        view returns(string[] memory) 
+    {
         return OwnerToData[wallet].tableIds;
     }
 
-    function claimRewards() external {
+    function claimRewards() 
+        external 
+    {
         require(rewards > 0, "You have no rewards to claim");
         rewardsToken.safeTransfer(msg.sender, rewards);
         emit Transfer(address(this), msg.sender, rewards);
+    }
+
+    function returnPrice(uint256 id) public view returns(uint256) {
+        return tokenPrices[id];
     }
 }
