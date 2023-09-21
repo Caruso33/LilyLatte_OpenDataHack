ef0dd317849c57c1d79e98726241c977306af085
Tobias Leinß <tobias.leinss@mway.io>
2023-09-21 12:05:41 +0200

    Add ownerlist

--- Token/contracts/LilyLatte.sol
+++ Token/contracts/LilyLatte.sol
@@ -76,6 +76,7 @@ contract LilyLatte is ERC1155, Ownable {
 
     /// @dev Mapping owner wallet to his data
     mapping(address => DataOwner) public ownerToData;
+    address[] public ownerList;
 
     /// @dev Mapping tableId to owner
     mapping(string => address) public tableIdToOwner;
@@ -136,6 +137,8 @@ contract LilyLatte is ERC1155, Ownable {
         ownerToData[msg.sender].tableId = tableId;
 
         tableIdToOwner[tableId] = msg.sender;
+
+        ownerList.push(msg.sender);
 
         emit OwnerAdded(msg.sender, tableId);
     }
